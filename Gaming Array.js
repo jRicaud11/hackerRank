f9092e428397844df3a58d0cbbf8a15bdd6096cb
Juan <ricaudjuan@hotmail.com>
2023-07-31 19:24:50 +1200

    Problem Solved

---
+++ Gaming Array.js
@@ -0,0 +1,19 @@
+let arr1 =  [1, 3, 5, 7, 9];
+let arr2 = [7, 4, 6, 5, 9];
+
+function gamingArray(arr) {
+    let maxNumber = 0;
+    let turnsPlayed = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > maxNumber) {
+            maxNumber = arr[i];
+            turnsPlayed++
+        }
+    }
+
+    return (turnsPlayed % 2) ? 'BOB' : 'ANDY'
+};
+
+console.log(gamingArray(arr1))
+console.log(gamingArray(arr2))
